c085b7932378a48fd76bbd62982615bda3f915c5
yuma-matsui <oshino.iiyo@gmail.com>
2021-03-06 19:29:59 +0900

    val
    .val()
    DOMから値の取得
    引数に値を指定した場合値を変更

--- udemy_js/chapter04_jquery/calc_tax/js/main.js
+++ udemy_js/chapter04_jquery/calc_tax/js/main.js
@@ -1,3 +1,15 @@
-$("#btnExec").click(function(){
+$(function(){
+
+	function calcTax(){
+		const price = $("#price").val();
+		const taxRate = $("#taxRate").val();
 	
+		const taxAmount = Math.floor( price * taxRate / 100 );
+		const totalPrice = taxAmount + price * 1;
+	
+		$("#taxAmount").val(taxAmount);
+		$("#taxIncluded").val(totalPrice);
+	};
+
+	$("#btnExec").click(calcTax);
 });
